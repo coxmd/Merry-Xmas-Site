ef9cb4929406e9d95b7c6fdeb025642f17b779a8
coxmd <coxmusyoki@gmail.com>
2022-12-26 12:53:13 +0300

    Completed navigation and navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,34 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*==== MENU SHOW ====*/
+/* Validate if constant exist */
+if(navToggle){
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*==== MENU HIDDEN ====*/
+/* Validate if constant exist */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu');
+    })
+}
 
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
 
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // when each nav__link is clicked the show-menu class is removed
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
